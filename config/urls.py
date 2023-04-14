12e59034797639bc9db6cf494173a0faf43b4869
heloisabett <heloisabettramosifc@gmail.com>
2023-04-14 10:22:45 -0300

    marca

--- config/urls.py
+++ config/urls.py
@@ -19,3 +19,17 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from garagem.views import MarcaViewSet
+
+router = DefaultRouter()
+router.register(r"marca", MarcaViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+]
