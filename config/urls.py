bfe13e1071eb686595412aaf2389af4303702fbb
heloisabett <heloisabettramosifc@gmail.com>
2023-04-28 10:18:24 -0300

    usv

--- config/urls.py
+++ config/urls.py
@@ -14,20 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet
+from garagem.views import AcessorioViewSet,CategoriaViewSet, CorViewSet, MarcaViewSet, VeiculoViewSet
 
 router = DefaultRouter()
-router.register(r"marca", MarcaViewSet)
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"cors", CorViewSet)
+router.register(r"marcas", MarcaViewSet)
+router.register(r"veiculos", VeiculoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- garagem/views.py
+++ garagem/views.py
@@ -4,9 +4,30 @@
 
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Marca
-from garagem.serializers import MarcaSerializer
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
+from garagem.serializers import AcessorioSerializer, CategoriaSerializer, CorSerializer, MarcaSerializer, VeiculoSerializer, VeiculoDetailSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer   
+
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer        
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return VeiculoDetailSerializer
+        return VeiculoSerializer 
