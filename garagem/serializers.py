d6acffef193bf0c13bac3265814f7f5497d78934
heloisabett <heloisabettramosifc@gmail.com>
2023-08-28 08:18:42 -0300

    serializer feito

--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,33 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-
-from garagem.models import Acessorio, Categoria,Cor, Marca, Veiculo
-
-class AcessorioSerializer(ModelSerializer):
-    class Meta:
-        model = Acessorio
-        fields = "__all__"
-
-class CategoriaSerializer(ModelSerializer):
-    class Meta:
-        model = Categoria
-        fields = "__all__"
-
-class CorSerializer(ModelSerializer):
-    class Meta:
-        model = Cor
-        fields = "__all__"   
- 
-
-class MarcaSerializer(ModelSerializer):
-    class Meta:
-        model = Marca
-        fields = "__all__"
-
-class VeiculoSerializer(ModelSerializer):
-    class Meta:
-        model = Veiculo
-        fields = "__all__"
-
+from garagem.models import Veiculo
 
 class VeiculoDetailSerializer(ModelSerializer):
     class Meta:
